3157da89a7a38c0c6f722f089fb792407fed67d9
soozanne <sato755@naver.com>
2018-08-08 17:16:47 +0900

    글쓰기 수정(2)

---
+++ DONGL_1 복사본/DONGL_1/UploadPostTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  UploadPostTableViewCell.swift
+//  DONGL_1
+//
+//  Created by 방문사용자 on 2018. 8. 8..
+//  Copyright © 2018년 HHBS. All rights reserved.
+//
+
+import UIKit
+
+class UploadPostTableViewCell: UITableViewCell {
+
+    @IBAction func UploadButton(_ sender: Any) {
+    }
+    @IBOutlet weak var NoteTextField: UITextView!
+    @IBOutlet weak var TravelDate: UILabel!
+    @IBOutlet weak var TravelCity: UILabel!
+    @IBOutlet weak var Schedule: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var title: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
